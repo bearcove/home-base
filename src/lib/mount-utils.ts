35518b8a8789efb25f006f4eed5fc924a628acd6
Amos Wenger <amos@bearcove.eu>
2025-08-09 14:26:35 +0200

    Add .svelte-component to each mounted component

--- src/lib/mount-utils.ts
+++ src/lib/mount-utils.ts
@@ -8,32 +8,33 @@ type Exports = Record<string, any>;
 type Target = MountOptions["target"];
 
 function targetToElement(target: Document | Element | ShadowRoot): Element {
-    if (target instanceof Document) {
-        return target.body;
-    } else if (target instanceof ShadowRoot) {
-        return target.host;
-    } else {
-        return target;
-    }
+  if (target instanceof Document) {
+    return target.body;
+  } else if (target instanceof ShadowRoot) {
+    return target.host;
+  } else {
+    return target;
+  }
 }
 
 export function mountOrShowError<Props extends Record<string, any> = Record<string, any>>(
-    component: Component<Props, Exports, any>,
-    options: MountOptions<Props>,
+  component: Component<Props, Exports, any>,
+  options: MountOptions<Props>,
 ) {
-    const element = targetToElement(options.target);
+  const element = targetToElement(options.target);
+  element.classList.add("svelte-component");
 
-    try {
-        const result = mount(component, options);
-        return result;
-    } catch (e: any) {
-        console.error("While mounting component", component, options);
-        console.error("Got error", e);
-        let markup =
-            "<p>Woops, some JavaScript broke. Details are in console, reach out if you want to!</p>";
-        if (window.location.protocol === "http:") {
-            markup += `<pre style="min-height: 8em; width: 100%; overflow: scroll; font-size: .8rem"><span style="color: red">${e}</span>\n${e.stack}</pre>`;
-        }
-        element.innerHTML = markup;
+  try {
+    const result = mount(component, options);
+    return result;
+  } catch (e: any) {
+    console.error("While mounting component", component, options);
+    console.error("Got error", e);
+    let markup =
+      "<p>Woops, some JavaScript broke. Details are in console, reach out if you want to!</p>";
+    if (window.location.protocol === "http:") {
+      markup += `<pre style="min-height: 8em; width: 100%; overflow: scroll; font-size: .8rem"><span style="color: red">${e}</span>\n${e.stack}</pre>`;
     }
+    element.innerHTML = markup;
+  }
 }
